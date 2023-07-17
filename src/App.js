29edad4936409fad925ffcd840d7ab6bb2b6e071
Rafaela Peres <62906605+rsanPeres@users.noreply.github.com>
2023-07-17 20:20:27 -0300

    add front

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BankStatement from './BankStatement';
+
+function App() {
+  return (
+    <div className="App">
+      <BankStatement />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import BankStatement from './BankStatement';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BankStatement />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
